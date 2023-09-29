d43efbd20e0fa53516c5c5ee16577516b5e01caa
Jide Oyelayo <JideOyelayo1@Gmail.com>
2023-09-29 16:54:53 +0100

    update

---
+++ ViewModel/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixelPowerPreInterviewAssessment.ViewModel
+{
+    internal class Class1
+    {
+    }
+}
